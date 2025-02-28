542da0bc6659b5da84f37c168b72fa2ce8025876
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-02-28 10:36:55 +0200

    55

--- module 03/55/script.js
+++ module 03/55/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const boxesQuery = document.querySelectorAll('.box');
+const boxesGet = document.getElementsByClassName('box');
+
+boxesQuery.forEach((box) => {
+   if (box.matches('.this')) {
+      console.log(box);
+   }
+});
+
+console.log(boxesQuery[0].closest('.wrapper'));
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// console.log(document.body.children);
+
+// console.log(Array.from(boxesGet));
