bb89cbeeee0a365f6a0e0755934ab852f3ee0603
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-05 16:40:17 +0200

    85-2

--- модуль 05/food/js/script.js
+++ модуль 05/food/js/script.js
@@ -227,7 +227,7 @@ window.addEventListener('DOMContentLoaded', () => {
    const forms = document.querySelectorAll('form');
 
    const message = {
-      loading: 'Загрузка',
+      loading: 'img/form/spinner.svg',
       success: 'Спасибо! Скоро мы с вами свяжемся',
       failure: 'Что-то пошло не так...',
    };
@@ -240,10 +240,13 @@ window.addEventListener('DOMContentLoaded', () => {
       form.addEventListener('submit', (e) => {
          e.preventDefault();
 
-         const statusMessage = document.createElement('div');
-         statusMessage.classList.add('status');
-         statusMessage.textContent = message.loading;
-         form.append(statusMessage);
+         const statusMessage = document.createElement('img');
+         statusMessage.src = message.loading;
+         statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+         `;
+         form.insertAdjacentElement('afterend', statusMessage);
 
          const request = new XMLHttpRequest();
          request.open('POST', 'server.php');
