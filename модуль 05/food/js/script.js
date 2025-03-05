d1e69b172b94c916e47828746adaed103c6c5ee8
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-05 12:07:32 +0200

    84-1

--- модуль 05/food/js/script.js
+++ модуль 05/food/js/script.js
@@ -225,4 +225,46 @@ window.addEventListener('DOMContentLoaded', () => {
       'menu__item',
    ).render();
 
+   // =============== FORMS ===============
+
+   const forms = document.querySelectorAll('form');
+
+   const message = {
+      loading: 'Загрузка',
+      success: 'Спасибо! Скоро мы с вами свяжемся',
+      failure: 'Что-то пошло не так...',
+   };
+
+   forms.forEach((form) => {
+      postData(form);
+   });
+
+   function postData(form) {
+      form.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
+         const request = new XMLHttpRequest();
+         request.open('POST', 'server.php');
+
+         // request.setRequestHeader('Content-type', 'multipart/form-data');
+         const formData = new FormData(form);
+
+         request.send(formData);
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
+      });
+   }
+
 });
