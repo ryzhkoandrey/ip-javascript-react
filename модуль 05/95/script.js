41d8982ac223e866f67d8eec7434ea728c0fd6fc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-08 22:44:10 +0200

    95-1

---
+++ модуль 05/95/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const checkbox = document.querySelector('#checkbox');
+const form = document.querySelector('form');
+const change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+   checkbox.checked = true;
+}
+
+checkbox.addEventListener('change', () => {
+   localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+
+});
