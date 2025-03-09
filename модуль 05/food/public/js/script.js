e95775af1e3a39912500ff43b8ff6e6940538d3d
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-09 19:42:54 +0200

    97-1

--- модуль 05/food/public/js/script.js
+++ модуль 05/food/public/js/script.js
@@ -361,4 +361,28 @@ window.addEventListener('DOMContentLoaded', () => {
       dots[slideIndex - 1].classList.add('dot--active');
    }
 
+   // =============== CALCULATOR ===============
+
+   const result = document.querySelector('.calculating__result span');
+   let sex;
+   let height;
+   let weight;
+   let age;
+   let ratio;
+
+   function calcTotal() {
+      if (!sex || !height || !weight || !age || ratio) {
+         result.textContent = '____';
+         return;
+      }
+
+      if (sex === 'female') {
+         result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+      } else {
+         result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+      }
+   }
+
+   calcTotal();
+
 });
