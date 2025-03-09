6fbb3f97b547a1989c67fe1a50ca98a3d2f7538a
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-09 11:21:57 +0200

    95-2

--- модуль 05/95/script.js
+++ модуль 05/95/script.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const checkbox = document.querySelector('#checkbox');
-const form = document.querySelector('form');
+const wrapper = document.querySelector('.wrapper');
 const change = document.querySelector('#color');
 
 if (localStorage.getItem('isChecked')) {
    checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg')) {
+   wrapper.style.backgroundColor = 'blue';
+} else {
+   wrapper.style.backgroundColor = '';
 }
 
 checkbox.addEventListener('change', () => {
@@ -13,5 +19,21 @@ checkbox.addEventListener('change', () => {
 });
 
 change.addEventListener('click', () => {
-
+   if (localStorage.getItem('bg')) {
+      localStorage.removeItem('bg');
+      wrapper.style.backgroundColor = '';
+   } else {
+      localStorage.setItem('bg', 'changed');
+      wrapper.style.backgroundColor = 'blue';
+   }
 });
+
+const persone = {
+   name: 'Andrey',
+   age: 37,
+}
+
+const serializedPerson = JSON.stringify(persone);
+localStorage.setItem('andrey', serializedPerson);
+
+console.log(JSON.parse(localStorage.getItem('andrey')));
