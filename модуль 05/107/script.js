e1ba2747b3242995a63d64336267d504bcd7a631
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-13 18:31:58 +0200

    107

---
+++ модуль 05/107/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const data = [
+   {
+      id: 'box',
+      tag: 'div'
+   },
+   {
+      id: 'f',
+      tag: 'nav'
+   },
+   {
+      id: 'circle',
+      tag: ''
+   }
+]
+
+try {
+   data.forEach((blockObj, i) => {
+      const block = document.createElement(blockObj.tag);
+
+      if (!blockObj.id) {
+         throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+      }
+
+      block.setAttribute('id', blockObj.id);
+      document.body.append(block);
+   });
+} catch (e) {
+   if (e.name === 'SyntaxError') {
+      console.log(e.message);
+   } else throw e;
+}
+
+
+
+console.log('next');
